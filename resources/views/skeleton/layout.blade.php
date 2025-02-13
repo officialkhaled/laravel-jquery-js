1f620cb27a5ccd8f4d9fc8a5d033064fbb8ba758
Khaled <khaled@skylarksoft.com>
2025-02-13 12:37:45 +0600

    footer fix

--- resources/views/skeleton/layout.blade.php
+++ resources/views/skeleton/layout.blade.php
@@ -147,7 +147,9 @@
     @yield('content')
 </div>
 
-@include('skeleton.footer')
+<div>
+    @include('skeleton.footer')
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
         integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
